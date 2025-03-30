4454655ba3d8fe9f840840b0976c20568f4de59c
Tanmay337442 <tanmay337442@gmail.com>
2025-03-30 17:59:16 -0400

    update

--- nav.js
+++ nav.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    $("#nav-placeholder").load("nav.html");
+    // Load the navbar from nav.html
+    $("#nav-placeholder").load("/nav.html", function() {
+        // After the navbar is loaded, add active class based on current page
+        var currentUrl = window.location.pathname;
+
+        // Loop through each nav-link in the loaded navbar and check if it matches the current URL
+        $('nav .nav-link').each(function() {
+            // If the href matches the current URL, add the 'active' class
+            if (this.href.includes(currentUrl)) {
+                $(this).addClass('active');
+            }
+        });
+    });
 });

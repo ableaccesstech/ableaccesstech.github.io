63e374136a93d021c0d5829edf6f1387a244d182
Tanmay337442 <tanmay337442@gmail.com>
2025-03-30 18:04:07 -0400

    update nav.js

--- nav.js
+++ nav.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
     // Load the navbar from nav.html
-    $("#nav-placeholder").load("/nav.html", function() {
+    $("#nav-placeholder").load("nav.html", function() {
         // After the navbar is loaded, add active class based on current page
         var currentUrl = window.location.pathname;
 
+        // First, remove the 'active' class from all nav links
+        $('nav .nav-link').removeClass('active');
+        
         // Loop through each nav-link in the loaded navbar and check if it matches the current URL
         $('nav .nav-link').each(function() {
             // If the href matches the current URL, add the 'active' class
